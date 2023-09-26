eb4f88e9fcd1946de60edfde679377943111afde
sabuj_sarker <raj16@cse.pstu.ac.bd>
2023-09-27 02:13:56 +0600

    This is my assignment 1

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,12 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product.js';
+export default function Products({ products }) {
+    return (
+        <div className="products">
+            {products.map((product, i) => (
+                <Product key={i} product={product} />
+            ))}
+        </div>
+    );
+}
